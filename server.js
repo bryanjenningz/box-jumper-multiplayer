f9692023f0dd0f2de876c951fb34bfd5818cd42d
Bryan Jennings <bryanjenningz@gmail.com>
2016-07-10 15:07:38 -0700

    add disconnected player server event

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ var io = require('socket.io')(http)
 app.use(express.static(__dirname + '/public'))
 
 io.on('connection', (socket) => {
+  console.log(socket.id + ' connected')
   socket.on('update player', (player) => {
     player = Object.assign({}, player, {id: socket.id.replace(/[\/#]/g, '')})
     socket.broadcast.emit('update player', player)
+  })
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('disconnected player', socket.id.replace(/[\/#]/g, ''))
+    console.log(socket.id + ' disconnected')
   })
 })
 
