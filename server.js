8f3c29610a0db5191b701ba8bfd1758a10df60a2
Bryan Jennings <bryanjenningz@gmail.com>
2016-07-10 13:58:41 -0700

    update player state to other connections server

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 var express = require('express')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
 app.use(express.static(__dirname + '/public'))
-app.listen(3000, () => console.log('Listening on port 3000.'))
+
+io.on('connection', (socket) => {
+  socket.on('update player', (player) => {
+    player = Object.assign({}, player, {id: socket.id.replace(/[\/#]/g, '')})
+    socket.broadcast.emit('update player', player)
+  })
+})
+
+http.listen(3000, () => console.log('Listening on port 3000.'))
